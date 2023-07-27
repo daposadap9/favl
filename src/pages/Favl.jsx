85d9fb733aa204586be8c09f5aceddc38094050b
johanserna <el.johan0511@hotmail.com>
2023-07-27 09:42:24 -0500

    new changes responsive login and changes favl

--- src/pages/Favl.jsx
+++ src/pages/Favl.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import TransitionEffect from "../components/transition/TransitionEffect";
 import axios from "axios";
